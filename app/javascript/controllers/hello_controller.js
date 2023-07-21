f3b5e3bc39f2cfdb5707d457d34dc340077c7b86
Daniel <danielcandidosilwa@gmail.com>
2023-07-21 10:55:36 -0300

    star layout with stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "<h1>Texto que vem do controller</h1>"
+    this.element.innerHTML = "<h1>Texto que vem do controller</h1>"
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :clientes
 
+  get 'stimulus', to: 'spa#index'
   root "home#index"
 end
